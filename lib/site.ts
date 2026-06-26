923c6ca3c9453c59f414b3fc3693429a3316e4f1
abdelhafidfstg <abdelhafidfstg@gmail.com>
2026-06-26 11:53:53 +0100

    Update site.ts

--- lib/site.ts
+++ lib/site.ts
@@ -6,7 +6,7 @@ export const site = {
   // Numéro WhatsApp au format international, sans "+" ni espaces.
   whatsapp: '0664740124',
   whatsappLabel: '+212 6 64 74 01 24',
-  email: 'contact@auriza.ma',
+  email: 'aurizaauriza80@gmail.com',
   instagram: 'https://www.instagram.com/auriza_hair?igsh=MW1vMHp0Nndyd2Zyag%3D%3D&utm_source=qr',
   tiktok: 'https://www.tiktok.com/@huile_auriza?_r=1&_t=ZS-977mNqQGiOo',
   // 📩 FormSubmit : remplacez par votre adresse e-mail réelle.
